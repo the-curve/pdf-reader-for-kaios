afff61c98fbad0aca550fd9638469f95a32e78e2
Curve <abhayemmanuel05@gmail.com>
2021-08-29 18:45:23 +0530

    improving

--- js/pdfHandler.js
+++ js/pdfHandler.js
@@ -308,11 +308,11 @@ document.addEventListener('keydown', (event) => {
     if (keyName == "*") {
         console.log(count)
         if (count == 0) {
-            canvas.style.filter = "invert(100%) sepia(20%) brightness(0.5)"
+            canvas.style.filter = "invert(100%) sepia(20%) brightness(0.7)"
             console.log("invert")
             count++;
         } else {
-            canvas.style.filter = "invert(0) sepia(0) brightness(1)"
+            canvas.style = ""
             console.log("reinvert")
             count--;
         }
